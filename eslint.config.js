b5c40ba854a7bc8eb9f77cb62f53c4dc5094144d
Fatima Macias <fatima.macias.us@gmail.com>
2025-07-09 15:37:14 -0400

    added ESLint and Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,15 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import { version } from 'react';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
+    
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
@@ -22,8 +25,20 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    plugins: {
+      react,
+      reactHooks
+    },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      'no-unused-vars' : 'warn',
+      'react/prop-types': 'off'
     },
   },
 ])
