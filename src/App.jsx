23b0d3a8ca5ffe68e59fed3bf1b6ce03dd2b8f30
Fatima Macias <fatima.macias.us@gmail.com>
2025-08-09 22:02:44 -0400

    week 05 -- Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,30 @@ import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 function App() {
   const [todoList, setTodoList] = useState([]);
+  
   function addTodo(title)
   {
     const newTodo = {
       id : Date.now(),
-      title
+      title,
+      isCompleted : false
     };
     setTodoList([...todoList,newTodo]);
   }
+
+  function completeTodo(id)
+  {
+    const updatedTodos = todoList.map((todo) =>
+      todo.id === id ? {...todo, isCompleted:true} : todo
+    );
+    setTodoList(updatedTodos);
+  }
+  
   return(
      <main>
       <Header/>
       <TodoForm onAddTodo={addTodo}/>
-      <TodoList todoList={todoList}></TodoList>
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo}></TodoList>
     </main>
   )
 }
